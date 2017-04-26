987bad1e3d5855b14b2a20459ffcc0f123a71d49
pejcairampoma <pejcairampoma@PELT0108573.galileo.ebel>
2017-04-26 18:04:09 -0500

    Example

---
+++ myTest/src/myTest/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package myTest;
+
+/**
+ * @author pejcairampoma
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println(" Initial");
+
+	}
+
+}
